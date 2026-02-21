62021cf56108a6959f37a5cba88e96d61731f7b4
reinaldorm <105465734+reinaldorm@users.noreply.github.com>
2026-02-21 13:18:49 -0300

    Refactor animation functions and improve naming

--- card/card_view.gd
+++ card/card_view.gd
@@ -33,7 +33,7 @@ func get_size() -> Vector2:
 	
 	return _size * 2.5
 
-func _animate(c: ANIMATION_CHANNEL, kill_previous: bool = true) -> Tween:
+func _get_tween_by_channel(c: ANIMATION_CHANNEL, kill_previous: bool = true) -> Tween:
 	var tween = _tween_channel[c]
 	
 	if tween: 
@@ -50,30 +50,27 @@ func _create_animation_callback(node: Node2D, tween: Tween) -> Callable:
 		tween.set_ease(ease)
 		tween.set_trans(transition)
 
-func animate_layout(parallel: bool = true, kill_previous: bool = true) -> Callable:
-	var tween = _animate(ANIMATION_CHANNEL.LAYOUT, kill_previous)
+func animate(channel: ANIMATION_CHANNEL, parallel: bool = true, kill_previous: bool = true) -> Callable:
+	var tween = _get_tween_by_channel(ANIMATION_CHANNEL, kill_previous)
 	tween.set_parallel(parallel)
 
-	return _create_animation_callback(self, tween)
+	var to_animate: Node2D
 
-	return animation_callback
+	match channel:
+		ANIMATION.CHANNEL.LAYOUT:
+			to_animate = self
+		ANIMATION.CHANNEL.GRAPHICS:
+			to_animate = sprite_2d
+
+	return _create_animation_callback(to_animate, tween)
  
 # -------------------------
 # Internal
 # -------------------------
 
 func _set_hovered(value: bool) -> void:
-	if drag_component.being_dragged: return
-	
-	var target_scale := Vector2(2.75, 2.75) if value else Vector2(2.5, 2.5)
-	animate_scale(target_scale)
-
-func _new_tween(e := Tween.EASE_OUT, t := Tween.TRANS_ELASTIC) -> Tween:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_ease(e)
-	tween.set_trans(t)
-	return tween
+	## Waiting new animation system to be tested
+	pass
 
 # -------------------------
 # Handlers
