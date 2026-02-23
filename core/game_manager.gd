dcb256d6e483c605f8c1eb0d0d9ee12aac5afae2
reinaldorm <105465734+reinaldorm@users.noreply.github.com>
2026-02-23 18:41:59 -0300

    refactor `CardData` and playing logic

--- core/game_manager.gd
+++ core/game_manager.gd
@@ -1,18 +1,20 @@
 extends Node
 class_name GameManager
 
-@export var hand : Hand
-@export var draw_pile_node : DrawPile
-@export var discard_pile_node : DiscardPile
-
-var discard_pile: Array[CardData] = []
-var draw_pile: Array[CardData] = []
-
 signal cards_played(card: Array[CardData])
 signal play_denied(card: Array[CardData])
-
 signal cards_drawn(card: Array[CardData])
-signal drawn_denied(card: Array[CardData])
+signal drawn_denied()
+
+@export var _turn_manager : TurnManager
+@export var _hue_manager : HueManager
+@export var _discard_pile_node : DiscardPile
+@export var _draw_pile_node : DrawPile
+@export var _hand : Hand
+
+var _discard_pile : Array[CardData] = []
+var _draw_pile : Array[CardData] = []
+var _payload := 0
 
 # -------------------------
 # Public API
@@ -21,19 +23,19 @@ signal drawn_denied(card: Array[CardData])
 func start() -> void:
 	var deck = _create_deck()
 	deck.shuffle()
-	draw_pile = deck
+	_draw_pile = deck
 	
-	var first_card = draw_pile.pop_back()
+	var first_card = _draw_pile.pop_back()
 	
-	discard_pile.append(first_card)
-	discard_pile_node.start(first_card)
+	_discard_pile.append(first_card)
+	_discard_pile_node.start(first_card)
 	
 	var player_deck : Array[CardData] = []
 	
 	for i in range(2):
 		player_deck.append(draw_pile.pop_back())
 	
-	hand.start(player_deck)
+	_hand.start(player_deck)
 
 # -------------------------
 # Internal
@@ -47,34 +49,67 @@ func _create_deck() -> Array[CardData]:
 	
 	## One 0 card for each color
 	for color in range(4):
-		var data = CardData.create(color, 0)
+		var data = CardData.create_numbered(color, 0)
 		new_deck.append(data)
 	
-## Two of each numbered card from 1-12 for each color
+	## Two of each numbered card from 1-9 for each color
 	for color in range(4):
 		for i in range(2):
-			for number in range(12):
-				var data = CardData.create(color, number)
+			for number in range(1, 9):
+				var data = CardData.create_numbered(color, number)
 				new_deck.append(data)
 	
+	## Two of each special card (SKIP, REVERSE, DRAW) for each color
+	for color in range(4):
+		for i in range(2):
+			var data_skip = CardData.create_special(color, CardData.Effect.SKIP)
+			var data_reverse = CardData.create_special(color, CardData.Effect.REVERSE)
+			var data_draw = CardData.create_special(color, CardData.Effect.DRAW, 2)
+			new_deck.append(data_skip)
+			new_deck.append(data_reverse)
+			new_deck.append(data_draw)
+
+	## Four of each special wild card (WILD, WILD FOUR) for each color
+	for color in range(4):
+		var data_wild = CardData.create_special(CardData.Hue.WILD, CardData.Effect.NULL)
+		var data_wild_four = CardData.create_special(CardData.Hue.WILD, CardData.Effect.DRAW, 4)
+		new_deck.append(data_wild)
+		new_deck.append(data_wild_four)
+	
 	return new_deck
+
+func _request_play_availability(card: CardData) -> bool:
+	## `card` will be the card which to evaluate.
+	## `last_card` is necessary here should we evaluate combo legibility.
+
+	var last_card = _discard_pile[_discard_pile.size() - 1]
+
+	if _payload:
+		if card.hue == CardData.Hue.WILD and card.effect == CardData.Effect.DRAW return true
+
+		if last_card.hue == CardData.Hue.WILD:
+			if card.effect == CardData.Effect.DRAW:
+				if card.hue == hue_manager.hue: return true
+		else: 
+			if card.effect == CardData.Effect.DRAW: return true
+		
+		return false
+
+	if card.hue == CardData.Hue.WILD: return true
+	if card.hue == last_card.hue: return true
+	if card.effect == last_card.effect: return true
+	if card.number == last_card.number: return true
+
+	return false
 
 # -------------------------
 # Handlers
 # -------------------------
 
 func _on_discard_pile_play_requested(cards: Array[CardData]) -> void:
-	var last_played_card = discard_pile[discard_pile.size() - 1]
-	var is_play_valid := false
+	var request_response := _request_play_availability(cards[0])
 	
-	if cards[0].number == last_played_card.number:
-		is_play_valid = true
-		print("cool UPSIDE")
-	elif cards[0].color == last_played_card.color:
-		is_play_valid = true
-		print("cool DOWNSIDE")
-		
-	if is_play_valid:
+	if request_response:
 		emit_signal("cards_played", cards)
 		discard_pile_node.accept_play_request()
 		discard_pile.append_array(cards)
@@ -85,7 +120,7 @@ func _on_discard_pile_play_requested(cards: Array[CardData]) -> void:
 func _on_draw_pile_draw_requested() -> void:
 	var drawn_cards : Array[CardData]
 	
-	for i in range(2): drawn_cards.append(draw_pile.pop_back())
+	for i in range(2): drawn_cards.append(_draw_pile.pop_back())
 	#draw_pile_node.draw_cards(drawn_cards)
 	
 	emit_signal("cards_drawn", drawn_cards)
---
+++ core/hue_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+class_name HueManager
+
+var hue : CardData.Hue
+
+var setup(_hue: CardData.Hue) -> void:
+    hue = _hue
