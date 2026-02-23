178f46e416656b3b0963224d87e68aad05016288
reinaldorm <naldo-mar@hotmail.com>
2026-02-23 13:48:43 -0300

    Add camera zoom tween & refactor idle tick
    Rename delta_count to _tick and add a _tween variable; use _tick for idle camera offset/turn arrow rotation. Introduce _zoom_to_pos that kills any existing tween and creates a new exponential ease tween to animate camera.zoom and camera.global_position. Add _on_draw_pile_draw_requested to zoom to the draw pile, and update the scene to connect DrawPile's draw_requested signal and adjust initial camera offset/TurnArrow rotation.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -5,13 +5,23 @@ class_name Main
 @export var camera : Camera2D
 @export var turn_arrow : Sprite2D
 
-var delta_count := 0.0
+var _tick := 0.0
+var _tween : Tween
 
 func _process(delta: float) -> void:
-	delta_count += delta
+	_tick += delta
 	_idle()
 
 func _idle() -> void:
 	var m_offset = (get_global_mouse_position() - camera.global_position) * 0.01
-	camera.offset = camera.offset.lerp( Vector2(cos(delta_count  * 0.75) * 1.5, sin(delta_count  * 0.75) * 1.5) + m_offset, 0.1)
-	turn_arrow.rotation = delta_count * 0.1
+	camera.offset = camera.offset.lerp( Vector2(cos(_tick  * 0.75) * 1.5, sin(_tick  * 0.75) * 1.5) + m_offset, 0.1)
+	turn_arrow.rotation = _tick * 0.1
+
+func _zoom_to_pos(pos: Vector2) -> void:
+	if _tween: _tween.kill()
+	_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).set_parallel()
+	_tween.tween_property(camera, "zoom", Vector2(2.0, 2.0), 1)
+	_tween.tween_property(camera, "global_position", pos, 1)
+	
+func _on_draw_pile_draw_requested() -> void:
+	_zoom_to_pos($DrawPile.global_position + Vector2(0, 25))
